22e81af9742d6ba007180af239c64be627334c69
AndrMenezes <andrefelipemaringa@gmail.com>
2022-10-06 16:20:19 +0100

    updating simulation study including the bias correction version of pearson coefficient

--- 03__modeling/2022-09-29__simulation_studies/models/simulation_study_trivariate_normal.R
+++ 03__modeling/2022-09-29__simulation_studies/models/simulation_study_trivariate_normal.R
@@ -2,9 +2,11 @@ rm(list = ls())
 library(dplyr)
 library(ggplot2)
 library(cowplot)
+library(parallel)
 theme_set(
   theme_cowplot() +
-    background_grid()
+    background_grid() +
+    theme(legend.position = "top")
 )
 
 res_path <- "./03__modeling/2022-09-29__simulation_studies/models/results"
@@ -28,6 +30,7 @@ n_max <- max(n_samples)
 # Function to calculate the pearson correlation coefficient
 compute_corr <- function(v_y) {
   p <- nrow(v_y)
+  n <- ncol(v_y)
   comb2v2 <- combn(p, 2)
   lt <- list()
   for (i in seq_len(ncol(comb2v2))) {
@@ -36,9 +39,12 @@ compute_corr <- function(v_y) {
                     v_y[comb2v2[, i][2L], ],
                     method = "pearson",
                     alternative = "two.sided")
+    r <- tmp$estimate
+    r_adj <- r * (1 + (1 - r^2) / 2 / n)
     lt[[i]] <- data.frame(
       comb = paste0(comb2v2[, i], collapse = " vs "),
-      rho = tmp$estimate,
+      rho = r,
+      rho_adj = r_adj,
       pvalue = tmp$p.value,
       ci_lower = if (is.null(tmp$conf.int[1L])) 0 else tmp$conf.int[1L],
       ci_upper = if (is.null(tmp$conf.int[2L])) 0 else tmp$conf.int[2L])
@@ -52,11 +58,7 @@ compute_corr <- function(v_y) {
 # Perform the simulation study
 if (!file.exists(file.path(res_path, "simulated_results.rds"))) {
   set.seed(6669)
-  counter <- 1L
-  list_results <- list()
-  for (rho in list_correlations) {
-    
-    cat(rho, "\n")
+  list_results <- mclapply(X = list_correlations, FUN = function(rho) {
     
     # Define the new correlation matrix
     R[lower.tri(R)] <- rho
@@ -76,11 +78,17 @@ if (!file.exists(file.path(res_path, "simulated_results.rds"))) {
       }))
     }, simplify = FALSE)
     end <- proc.time()
-    
-    cat("\n Finished in", (end[3L] - ini[3L]) / 60, "minutes\n")  
-    list_results[[counter]] <- do.call(rbind, out)
-    counter <- counter + 1L
-  }
+    # Tracking the scenarios 
+    write.table(
+      x = data.frame(paste0(rho, collapse = ", "), (end[3L] - ini[3L]) / 60),
+      file = file.path(res_path, "tracking.txt"), 
+      sep = "\t",
+      append = TRUE,
+      col.names = FALSE, 
+      row.names = FALSE,
+      quote = FALSE)
+    return(do.call(rbind, out))
+  }, mc.cores = 3L)
   names(list_results) <- names(list_correlations)
   data_results <- bind_rows(list_results, .id = "scenario") |> 
     as_tibble()
@@ -106,23 +114,29 @@ counter <- 1L
 list_plots <- list()
 for (chosen in unique(data_results$scenario)) {
   data_curr <- data_results |> 
-    filter(scenario == chosen)
-  
+    filter(scenario == chosen) |> 
+    select(comb, n_sample, rho, rho_adj) |> 
+    tidyr::pivot_longer(cols = -c(comb, n_sample))
+
   aux <- list("scenario_1" = "Low (0.25)",
               "scenario_2" = "Moderate (0.50)",
               "scenario_3" = "High (0.85)")[[chosen]]
   
-  list_plots[[counter]] <- ggplot(data_curr, aes(x = n_sample, y = rho,
-                                                 group = n_sample)) +
+  list_plots[[counter]] <- ggplot(data_curr, aes(x = n_sample, y = value,
+                                                 fill = name)) +
     facet_wrap(~ comb) +
     geom_boxplot() +
     geom_hline(data = filter(data_true_values, scenario == chosen),
-               aes(yintercept = true_rho), col = "red") +
-    labs(x = "Sample size", y = expression(rho)) +
+               aes(yintercept = true_rho), col = "red", size = 1) +
+    labs(x = "Sample size", y = "Estimate", fill = "") +
     ggtitle(expression("MC distribution of"~rho),
             paste0("Scenario: ", aux, " correlation")) +
-    scale_y_continuous(breaks = scales::pretty_breaks(6))
-  
+    scale_y_continuous(breaks = scales::pretty_breaks(6)) +
+    colorspace::scale_fill_discrete_qualitative(
+      labels = c("rho" = expression(rho), "rho_adj" = expression(rho[adj])))
+  fname <- paste0("boxplot_estimates__", counter, ".png")
+  save_plot(filename = file.path(res_path, fname),
+            plot = list_plots[[counter]], bg = "white", base_height = 6)
   counter <- counter + 1L
 }
 p_grid <- plot_grid(plotlist = list_plots)
@@ -132,45 +146,68 @@ save_plot(filename = file.path(res_path, "boxplot_estimates.png"),
 ###############################################################################
 # Summarizing the data
 data_summarized <- data_results |> 
-  group_by(scenario, n_sample, comb) |> 
-  summarise(mean_rho = mean(rho),
-            sd_rho = sd(rho),
-            relative_bias_rho = mean((rho - true_rho) / true_rho),
+  select(scenario, comb, n_sample, true_rho, rho, rho_adj) |> 
+  tidyr::pivot_longer(cols = -c(scenario, comb, n_sample, true_rho),
+                      names_to = "estimator",
+                      values_to = "value") |> 
+  group_by(scenario, n_sample, comb, estimator) |> 
+  summarise(mean = mean(value),
+            sd = sd(value),
             true_rho = mean(true_rho),
-            test_size = mean(pvalue <= 0.05),
-            coverage = mean(true_rho >= ci_lower & true_rho <= ci_upper),
+            relative_bias = mean((value - true_rho) / true_rho),
             .groups = "drop") |> 
   mutate(n_sample = forcats::fct_relevel(
            factor(n_sample), "3", "5", "10", "15"),
          comb_rho = paste0(comb, "  ", expression(rho), "=", true_rho))
 
 for (chosen in unique(data_summarized$scenario)) {
+
+  aux <- list("scenario_1" = "Low (0.25)",
+              "scenario_2" = "Moderate (0.50)",
+              "scenario_3" = "High (0.85)")[[chosen]]
+
+  cat(chosen, "\n")
   data_curr <- data_summarized |> 
     filter(scenario == chosen)
-  
-  p_mean <- ggplot(data_curr, aes(x = n_sample, y = mean_rho)) +
-    facet_wrap(. ~ comb_rho, scales = "free_y") +
+
+  p_mean <- ggplot(data_curr, aes(x = n_sample, y = mean, col = estimator,
+                                  group = estimator)) +
+    facet_wrap(. ~ comb) +
     geom_point(size = 3) +
+    geom_line(linetype = "dotted") +
     geom_hline(data = filter(data_true_values, scenario == chosen),
                aes(yintercept = true_rho), col = "red") +
-    labs(x = "Sample size", y = "Mean") +
-    ggtitle("Mean of the estimates correlation coeficient across the Monte Carlo replicates") +
-    scale_y_continuous(breaks = scales::pretty_breaks(6))
-  
-  p_bias <- ggplot(data_curr, aes(x = n_sample, y = relative_bias_rho)) +
-    facet_wrap(. ~ comb, scales = "free_y") +
+    labs(x = "Sample size", y = "Mean", col = "") +
+    ggtitle("Mean of the estimates correlation coeficient across the Monte Carlo replicates",
+            paste0("Scenario: ", aux, " correlation")) +
+    scale_y_continuous(breaks = scales::pretty_breaks(6)) +
+    colorspace::scale_color_discrete_qualitative(
+      labels = c("rho" = expression(rho), "rho_adj" = expression(rho[adj])))
+
+  p_bias <- ggplot(data_curr, aes(x = n_sample, y = relative_bias,
+                                  col = estimator, group = estimator)) +
+    facet_wrap(. ~ comb) +
     geom_point(size = 4) +
+    geom_line(linetype = "dotted") +
     geom_hline(yintercept = 0, col = "red") +
     labs(x = "Sample size", y = "Relative bias") +
-    ggtitle("Relative bias of the estimates correlation coeficient across the Monte Carlo replicates") +
-    scale_y_continuous(breaks = scales::pretty_breaks(6))
-
-  p_sd <- ggplot(data_curr, aes(x = n_sample, y = sd_rho)) +
-    facet_wrap(. ~ comb, scales = "free_y") +
+    ggtitle("Relative bias of the estimates correlation coeficient across the Monte Carlo replicates",
+            paste0("Scenario: ", aux, " correlation")) +
+    scale_y_continuous(breaks = scales::pretty_breaks(6)) +
+    colorspace::scale_color_discrete_qualitative(
+      labels = c("rho" = expression(rho), "rho_adj" = expression(rho[adj])))
+  
+  p_sd <- ggplot(data_curr, aes(x = n_sample, y = sd,
+                                col = estimator, group = estimator)) +
+    facet_wrap(. ~ comb) +
     geom_point(size = 4) +
+    geom_line(linetype = "dotted") +
     labs(x = "Sample size", y = "Standard Deviation") +
-    ggtitle("Standard deviation of the estimates correlation coeficient across the Monte Carlo replicates") +
-    scale_y_continuous(breaks = scales::pretty_breaks(6))
+    ggtitle("Standard deviation of the estimates correlation coeficient across the Monte Carlo replicates",
+            paste0("Scenario: ", aux, " correlation")) +
+    scale_y_continuous(breaks = scales::pretty_breaks(6)) +
+    colorspace::scale_color_discrete_qualitative(
+      labels = c("rho" = expression(rho), "rho_adj" = expression(rho[adj])))
 
   save_plot(
     filename = file.path(res_path, paste0("mean_estimate__", chosen, ".png")),
