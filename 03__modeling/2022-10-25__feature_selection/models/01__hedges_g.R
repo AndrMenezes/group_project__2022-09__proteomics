f316b8ecefcfb6a44971cbacd38f2dcc09f45b88
C-Monaghan <cormacmonaghan@tutanota.com>
2022-11-02 20:30:23 +0000

    Code formatting

--- 03__modeling/2022-10-25__feature_selection/models/01__hedges_g.R
+++ 03__modeling/2022-10-25__feature_selection/models/01__hedges_g.R
@@ -9,30 +9,33 @@ path_data <- "./03__modeling/2022-10-14__workflow/processing/processed_data"
 # First we import the QFeatures Data (all of it)
 fts <- readRDS(file.path(path_data, "fts_processed.rds"))
 
-# We can then focus at the protein level (fts[["proteins"]])
-se_protein <- fts[["proteins"]]
-colData(se_protein) <- colData(fts)
-
-# Finally, we can focus at the normalised protein data
-normalised_proteins <- assay(x = se_protein, i = "log_intensity_normalized")
-colnames(normalised_proteins) <- se_protein@colData@listData[["group"]]
-
 # Calculating Hedges G --------------------------------------------------------
-ampicillin <- apply(normalised_proteins, 1, function(x) 
-  hedges_g(x[1:3], x[10:12])$Hedges_g)
+ampicillin <- apply(
+  assay(fts[["proteins"]], i = "log_intensity_normalized"), 1,
+  function(z) hedges_g(z[1:3], z[10:12])$Hedges_g)
 
-cefotaxime <- apply(normalised_proteins, 1, function(x)
-  hedges_g(x[4:6], x[10:12])$Hedges_g)
+cefotaxime <- apply(
+  assay(fts[["proteins"]], i = "log_intensity_normalized"), 1,
+  function(x) hedges_g(x[4:6], x[10:12])$Hedges_g)
 
-ciprofloxacin <- apply(normalised_proteins, 1, function(x)
-  hedges_g(x[7:9], x[10:12])$Hedges_g)
+ciprofloxacin <- apply(
+  assay(fts[["proteins"]], i = "log_intensity_normalized"), 1,
+  function(x) hedges_g(x[7:9], x[10:12])$Hedges_g)
 
-impipenem <- apply(normalised_proteins, 1, function(x)
-  hedges_g(x[13:15], x[10:12])$Hedges_g)
+impipenem <- apply(
+  assay(fts[["proteins"]], i = "log_intensity_normalized"), 1,
+  function(x) hedges_g(x[13:15], x[10:12])$Hedges_g)
 
-# Creating matrix with all values
+# Organize the results in a data frame ----------------------------------------
 effect_sizes <- data.frame(ampicillin, cefotaxime, ciprofloxacin, impipenem)
-effect_sizes <- as.matrix(effect_sizes)
+colnames(effect_sizes) <- paste0("hedges_g__", colnames(effect_sizes))
+
+# Concatenating ---------------------------------------------------------------
+if (all.equal(rownames(fts[["proteins"]]), rownames(effect_sizes)))
+  rowData(fts[["proteins"]]) <- cbind(rowData(fts[["proteins"]]), effect_sizes)
+
+# Exporting -------------------------------------------------------------------
+saveRDS(object = fts, file = file.path(path_data, "fts_processed.rds"))
 
 ######################################
 #       Hedges G Formula
@@ -60,27 +63,28 @@ effect_sizes <- as.matrix(effect_sizes)
 #           Sanity Check
 ######################################
 # Using Formula on Protein 1 for Ampicillin vs. Control ------------------------
+# normalised_proteins <- assay(fts[["proteins"]], i = "log_intensity_normalized")
 # Getting means
-m_1 <- mean(normalised_proteins[1, 1:3])
-m_2 <- mean(normalised_proteins[1, 10:12])
+# m_1 <- mean(normalised_proteins[1, 1:3])
+# m_2 <- mean(normalised_proteins[1, 10:12])
 
 # Getting standard deviations & squaring
-sd_1 <- sd(normalised_proteins[1, 1:3])
-sd_2 <- sd(normalised_proteins[1, 10:12])
-
-sd_1 <- sd_1^2
-sd_2 <- sd_2^2
+# sd_1 <- sd(normalised_proteins[1, 1:3])
+# sd_2 <- sd(normalised_proteins[1, 10:12])
+# 
+# sd_1 <- sd_1^2
+# sd_2 <- sd_2^2
 
 # Calculating pooled standard deviation
-pooled_sd <- sqrt((sd_1 + sd_2)/2)
+# pooled_sd <- sqrt((sd_1 + sd_2)/2)
 
 # Calculating Cohen's D
-D <- (m_1 - m_2) / pooled_sd
+# D <- (m_1 - m_2) / pooled_sd
 
 # Applying Hedges Correction
-G <- D * (1 - (3/(4*(3+3) - 9)))
+# G <- D * (1 - (3/(4*(3+3) - 9)))
 
-G
+# G
 
 # Using Hedges G function on Protein 1 for Ampicillin vs. Control -------------
-hedges_g(normalised_proteins[1, 1:3], normalised_proteins[1, 10:12])$Hedges_g
+# hedges_g(normalised_proteins[1, 1:3], normalised_proteins[1, 10:12])$Hedges_g
